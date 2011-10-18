8ea3a99211a21dfe28bea878715899a097058964
Christer Edvartsen <cogo@starzinger.net>
2011-10-19 00:28:01 +0200

    Added an image url component

---
+++ library/ImboClient/ImageUrl/ImageUrl.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * ImboClient
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package ImboClient
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imboclient-php
+ */
+
+namespace ImboClient\ImageUrl;
+
+/**
+ * Image url implementation
+ *
+ * @package ImboClient
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imboclient-php
+ */
+class ImageUrl implements ImageUrlInterface {
+    /**
+     * Baseurl
+     *
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * The public key
+     *
+     * @var string
+     */
+    private $publicKey;
+
+    /**
+     * The image identifier
+     *
+     * @var string
+     */
+    private $imageIdentifier;
+
+    /**
+     * Query data
+     *
+     * @var string[]
+     */
+    private $data;
+
+    /**
+     * Class constructor
+     *
+     * @param string $baseUrl The baseurl to the server
+     * @param string $publicKey The public key to use
+     * @param string $imageIdentifier The image identifier
+     */
+    public function __construct($baseUrl, $publicKey, $imageIdentifier) {
+        $this->baseUrl = rtrim($baseUrl, '/');
+        $this->publicKey = $publicKey;
+        $this->imageIdentifier = $imageIdentifier;
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::border()
+     */
+    public function border($color = '000', $width = 1, $height = 1) {
+        return $this->append(sprintf('border:color=%s,width=%d,height=%d', $color, $width, $height));
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::compress()
+     */
+    public function compress($quality = 75) {
+        return $this->append('compress:quality=' . (int) $quality);
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::convert()
+     */
+    public function convert($type) {
+        $this->imageIdentifier = substr($this->imageIdentifier, 0, 32) . '.' . $type;
+
+        return $this;
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::gif()
+     */
+    public function gif() {
+        return $this->convert('gif');
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::jpg()
+     */
+    public function jpg() {
+        return $this->convert('jpg');
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::png()
+     */
+    public function png() {
+        return $this->convert('png');
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::crop()
+     */
+    public function crop($x, $y, $width, $height) {
+        return $this->append(sprintf('crop:x=%d,y=%d,width=%d,height=%d', $x, $y, $width, $height));
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::flipHorizontally()
+     */
+    public function flipHorizontally() {
+        return $this->append('flipHorizontally');
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::flipVertically()
+     */
+    public function flipVertically() {
+        return $this->append('flipVertically');
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::resize()
+     */
+    public function resize($width = null, $height = null) {
+        $params = array();
+
+        if ($width) {
+            $params[] = 'width=' . (int) $width;
+        }
+
+        if ($height) {
+            $params[] = 'height=' . (int) $height;
+        }
+
+        return $this->append('resize:' . implode(',', $params));
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::rotate()
+     */
+    public function rotate($angle, $bg = '000') {
+        return $this->append(sprintf('rotate:angle=%d,bg=%s', $angle, $bg));
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::thumbnail()
+     */
+    public function thumbnail($width = 50, $height = 50, $fit = 'outbound') {
+        return $this->append(sprintf('thumbnail:width=%d,height=%s,fit=%s', $width, $height, $fit));
+    }
+
+    /**
+     * @see ImboClient\ImageUrl\ImageUrlInterface::__toString()
+     */
+    public function __toString() {
+        $url = $this->baseUrl . '/' . $this->publicKey . '/' . $this->imageIdentifier;
+
+        if (empty($this->data)) {
+            return $url;
+        }
+
+        return $url . '?' . $this->getQueryString();
+    }
+
+    /**
+     * Append a string to the query
+     *
+     * @param string $part The string to append
+     * @return ImboClient\ImageUrl\ImageUrlInterface
+     */
+    private function append($part) {
+        $this->data[] = $part;
+
+        return $this;
+    }
+
+    /**
+     * Return the query string
+     *
+     * @return string
+     */
+    private function getQueryString() {
+        $query = null;
+        $query = array_reduce($this->data, function($query, $element) {
+            return $query . 't[]=' . $element . '&';
+        }, $query);
+        $query = rtrim($query, '&');
+
+        return $query;
+    }
+}
---
+++ tests/ImboClient/ImageUrl/ImageUrlTest.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * ImboClient
+ *
+ * Copyright (c) 2011 Christer Edvartsen <cogo@starzinger.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package ImboClient
+ * @subpackage Unittests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imboclient-php
+ */
+
+namespace ImboClient\ImageUrl;
+
+/**
+ * @package ImboClient
+ * @subpackage Unittests
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @copyright Copyright (c) 2011, Christer Edvartsen
+ * @license http://www.opensource.org/licenses/mit-license MIT License
+ * @link https://github.com/christeredvartsen/imboclient-php
+ */
+class ImageUrlTest extends \PHPUnit_Framework_TestCase {
+    private $url;
+    private $baseUrl = 'http://host';
+    private $publicKey = '3d08c65cb5509a0b2a909f3d5b56da08';
+    private $imageIdentifier = '83dfab4b5c2678e5f195ea21c5e6750b';
+
+    public function setUp() {
+        $this->url = new ImageUrl($this->baseUrl, $this->publicKey, $this->imageIdentifier);
+    }
+
+    public function tearDown() {
+        $this->url = null;
+    }
+
+    public function testBorder() {
+        $this->assertSame($this->url, $this->url->border());
+        $this->assertStringEndsWith('?t[]=border:color=000,width=1,height=1', (string) $this->url);
+    }
+
+    public function testBorderWithCustomValues() {
+        $this->assertSame($this->url, $this->url->border('fff', 2, 3));
+        $this->assertStringEndsWith('?t[]=border:color=fff,width=2,height=3', (string) $this->url);
+    }
+
+    public function testCompess() {
+        $this->assertSame($this->url, $this->url->compress());
+        $this->assertStringEndsWith('?t[]=compress:quality=75', (string) $this->url);
+    }
+
+    public function testCompressWithCustomValues() {
+        $this->assertSame($this->url, $this->url->compress(42));
+        $this->assertStringEndsWith('?t[]=compress:quality=42', (string) $this->url);
+    }
+
+    public function testConvert() {
+        $this->assertSame($this->url, $this->url->convert('jpg'));
+        $this->assertStringStartsWith($this->baseUrl . '/' . $this->publicKey . '/' . $this->imageIdentifier . '.jpg', (string) $this->url);
+    }
+
+    public function testGif() {
+        $this->assertSame($this->url, $this->url->gif());
+        $this->assertStringStartsWith($this->baseUrl . '/' . $this->publicKey . '/' . $this->imageIdentifier . '.gif', (string) $this->url);
+    }
+
+    public function testJpg() {
+        $this->assertSame($this->url, $this->url->jpg());
+        $this->assertStringStartsWith($this->baseUrl . '/' . $this->publicKey . '/' . $this->imageIdentifier . '.jpg', (string) $this->url);
+    }
+
+    public function testPng() {
+        $this->assertSame($this->url, $this->url->png());
+        $this->assertStringStartsWith($this->baseUrl . '/' . $this->publicKey . '/' . $this->imageIdentifier . '.png', (string) $this->url);
+    }
+
+    public function testCrop() {
+        $this->assertSame($this->url, $this->url->crop(1, 2, 3, 4));
+        $this->assertStringEndsWith('?t[]=crop:x=1,y=2,width=3,height=4', (string) $this->url);
+    }
+
+    public function testFlipHorizontally() {
+        $this->assertSame($this->url, $this->url->flipHorizontally());
+        $this->assertStringEndsWith('?t[]=flipHorizontally', (string) $this->url);
+    }
+
+    public function testFlipVertically() {
+        $this->assertSame($this->url, $this->url->flipVertically());
+        $this->assertStringEndsWith('?t[]=flipVertically', (string) $this->url);
+    }
+
+    public function testResizeWithOnlyWidth() {
+        $this->assertSame($this->url, $this->url->resize(100));
+        $this->assertStringEndsWith('?t[]=resize:width=100', (string) $this->url);
+    }
+
+    public function testResizeWithOnlyHeight() {
+        $this->assertSame($this->url, $this->url->resize(null, 100));
+        $this->assertStringEndsWith('?t[]=resize:height=100', (string) $this->url);
+    }
+
+    public function testResize() {
+        $this->assertSame($this->url, $this->url->resize(1, 2));
+        $this->assertStringEndsWith('?t[]=resize:width=1,height=2', (string) $this->url);
+    }
+
+    public function testRotate() {
+        $this->assertSame($this->url, $this->url->rotate(42));
+        $this->assertStringEndsWith('?t[]=rotate:angle=42,bg=000', (string) $this->url);
+    }
+
+    public function testRotateWithBg() {
+        $this->assertSame($this->url, $this->url->rotate(42, 'fff'));
+        $this->assertStringEndsWith('?t[]=rotate:angle=42,bg=fff', (string) $this->url);
+    }
+
+    public function testThumbnailWithAllParams() {
+        $this->assertSame($this->url, $this->url->thumbnail(1, 2, 'inset'));
+        $this->assertStringEndsWith('?t[]=thumbnail:width=1,height=2,fit=inset', (string) $this->url);
+    }
+}
