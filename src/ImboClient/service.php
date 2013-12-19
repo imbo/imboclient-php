f550e2e7716dd3e6e84e633e84adf4bdd7b9f417
Christer Edvartsen <cogo@starzinger.net>
2013-12-19 17:04:23 +0100

    Added more operations and in-line models. Missing a few commands before it's complete

--- src/ImboClient/service.php
+++ src/ImboClient/service.php
@@ -10,28 +10,20 @@
 
 return array(
     'name' => 'Imbo',
-    'apiVersion' => '',
-    'description' => 'Imbo has an API that allows you to add/remove images and metadata. The API also supports dynamically transforming images without storing all variants',
-
-    // API operations
+    'apiVersion' => '~1.0',
+    'description' => 'Imbo\'s API allows clients to add/delete images and add/update/delete metadata attached to these images',
     'operations' => array(
-        // Stats
         'GetServerStats' => array(
             'httpMethod' => 'GET',
             'uri' => '/stats.json',
             'summary' => 'Get statistics',
-            'responseClass' => 'ImboClient\Model\Stats',
         ),
-
-        // Status
         'GetServerStatus' => array(
             'httpMethod' => 'GET',
             'uri' => '/status.json',
             'summary' => 'Get status about the Imbo host',
-            'responseClass' => 'ImboClient\Model\Status',
+            'responseClass' => 'GetServerStatus',
         ),
-
-        // User
         'GetUserInfo' => array(
             'httpMethod' => 'GET',
             'uri' => '/users/{publicKey}.json',
@@ -44,15 +36,19 @@ return array(
                     'description' => 'The public key of the user we want information about',
                 ),
             ),
-            'responseClass' => 'ImboClient\Model\User',
+            'responseClass' => 'GetUserInfo',
         ),
-
-        // Image
         'AddImage' => array(
-            'httpMethod' => 'PUT',
-            'uri' => '/users/{publicKey}/images/{imageIdentifier}',
+            'httpMethod' => 'POST',
+            'uri' => '/users/{publicKey}/images',
             'summary' => 'Add an image',
             'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
                 'image' => array(
                     'type' => 'string',
                     'required' => true,
@@ -60,65 +56,270 @@ return array(
                     'description' => 'The binary data of the image to add',
                 ),
             ),
-            'responseClass' => 'ImboClient\Model\AddImage',
+            'responseClass' => 'AddImage',
         ),
-
         'DeleteImage' => array(
             'httpMethod' => 'DELETE',
             'uri' => '/users/{publicKey}/images/{imageIdentifier}',
             'summary' => 'Delete an image',
-            'responseClass' => 'ImboClient\Model\DeleteImage',
+            'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
+                'imageIdentifier' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The image identifier',
+                ),
+            ),
+            'responseClass' => 'DeleteImage',
         ),
-
-        'GetShortUrl' => array(
-            'httpMethod' => 'HEAD',
-            'uri' => '/users/{publicKey}/images/{imageIdentifier}',
-            'summary' => 'Get the short URL of an image',
-            'responseClass' => 'ImboClient\Model\ShortUrl',
-        ),
-
         'GetImageProperties' => array(
             'httpMethod' => 'HEAD',
             'uri' => '/users/{publicKey}/images/{imageIdentifier}',
             'summary' => 'Get properties of an image',
-            'responseClass' => 'ImboClient\Model\ImageProperties',
+            'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
+                'imageIdentifier' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The image identifier',
+                ),
+            ),
+            'responseClass' => 'GetImageProperties',
         ),
-
-        // Images
-        'GetImages' => array(
-            'httpMethod' => 'GET',
-            'uri' => '/users/{publicKey}/images',
-            'summary' => 'Query images',
-            'responseClass' => 'ImboClient\Model\ImageCollection',
-        ),
-
-        // Metadata
-        'GetMetadata' => array(
-            'httpMethod' => 'GET',
-            'uri' => '/users/{publicKey}/images/{imageIdentifier}/metadata',
-            'summary' => 'Get metadata attached to an image',
-            'responseClass' => 'ImboClient\Model\ImageMetadata',
-        ),
-
-        'ReplaceMetadata' => array(
-            'httpMethod' => 'PUT',
-            'uri' => '/users/{publicKey}/images/{imageIdentifier}/metadata',
-            'summary' => 'Completely replace the metadata attached to an image with new metadata',
-            'responseClass' => 'ImboClient\Model\ReplaceMetadata',
-        ),
-
         'EditMetadata' => array(
             'httpMethod' => 'POST',
             'uri' => '/users/{publicKey}/images/{imageIdentifier}/metadata',
             'summary' => 'Update metadata attached to an image. Supports partial updates',
-            'responseClass' => 'ImboClient\Model\EditMetadata',
+            'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
+                'imageIdentifier' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The image identifier',
+                ),
+                'metadata' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'body',
+                    'description' => 'Metadata presented as JSON',
+                ),
+            ),
+            'responseClass' => 'EditMetadata',
         ),
-
+        'GetMetadata' => array(
+            'httpMethod' => 'GET',
+            'uri' => '/users/{publicKey}/images/{imageIdentifier}/metadata.json',
+            'summary' => 'Get metadata attached to an image',
+            'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
+                'imageIdentifier' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The image identifier',
+                ),
+            ),
+        ),
+        'ReplaceMetadata' => array(
+            'httpMethod' => 'PUT',
+            'uri' => '/users/{publicKey}/images/{imageIdentifier}/metadata',
+            'summary' => 'Completely replace the metadata attached to an image with new metadata',
+            'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
+                'imageIdentifier' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The image identifier',
+                ),
+                'metadata' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'body',
+                    'description' => 'Metadata presented as JSON',
+                ),
+            ),
+            'responseClass' => 'ReplaceMetadata',
+        ),
         'DeleteMetadata' => array(
             'httpMethod' => 'DELETE',
             'uri' => '/users/{publicKey}/images/{imageIdentifier}/metadata',
             'summary' => 'Delete metadata attached to an image',
-            'responseClass' => 'ImboClient\Model\DeleteMetadata',
+            'parameters' => array(
+                'publicKey' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The owner of the image',
+                ),
+                'imageIdentifier' => array(
+                    'type' => 'string',
+                    'required' => true,
+                    'location' => 'uri',
+                    'description' => 'The image identifier',
+                ),
+            ),
+            'responseClass' => 'DeleteMetadata',
+        ),
+    ),
+    'models' => array(
+        'GetServerStatus' => array(
+            'type' => 'array',
+            'properties' => array(
+                'status' => array(
+                    'location' => 'statusCode',
+                    'type' => 'integer',
+                ),
+                'message' => array(
+                    'location' => 'reasonPhrase',
+                    'type' => 'string',
+                ),
+                'date' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                    'filters' => array(
+                        'date_create',
+                    ),
+                ),
+            ),
+        ),
+        'GetUserInfo' => array(
+            'type' => 'array',
+            'properties' => array(
+                'publicKey' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+                'numImages' => array(
+                    'location' => 'json',
+                    'type' => 'integer',
+                ),
+                'lastModified' => array(
+                    'location' => 'json',
+                    'type' => 'datetime',
+                    'filters' => array(
+                        'date_create',
+                    ),
+                ),
+            ),
+        ),
+        'AddImage' => array(
+            'type' => 'array',
+            'properties' => array(
+                'imageIdentifier' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+                'width' => array(
+                    'location' => 'json',
+                    'type' => 'integer',
+                ),
+                'height' => array(
+                    'location' => 'json',
+                    'type' => 'integer',
+                ),
+                'extension' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+            ),
+        ),
+        'DeleteImage' => array(
+            'type' => 'array',
+            'properties' => array(
+                'imageIdentifier' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+            ),
+        ),
+        'EditMetadata' => array(
+            'type' => 'array',
+            'properties' => array(
+                'imageIdentifier' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+            ),
+        ),
+        'ReplaceMetadata' => array(
+            'type' => 'array',
+            'properties' => array(
+                'imageIdentifier' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+            ),
+        ),
+        'DeleteMetadata' => array(
+            'type' => 'array',
+            'properties' => array(
+                'imageIdentifier' => array(
+                    'location' => 'json',
+                    'type' => 'string',
+                ),
+            ),
+        ),
+        'GetImageProperties' => array(
+            'type' => 'array',
+            'properties' => array(
+                'width' => array(
+                    'location' => 'header',
+                    'type' => 'integer',
+                    'sentAs' => 'x-imbo-originalwidth',
+                    'filters' => array('intval'),
+                ),
+                'height' => array(
+                    'location' => 'header',
+                    'type' => 'integer',
+                    'sentAs' => 'x-imbo-originalheight',
+                    'filters' => array('intval'),
+                ),
+                'filesize' => array(
+                    'location' => 'header',
+                    'type' => 'integer',
+                    'sentAs' => 'x-imbo-originalfilesize',
+                    'filters' => array('intval'),
+                ),
+                'extension' => array(
+                    'location' => 'header',
+                    'type' => 'string',
+                    'sentAs' => 'x-imbo-originalextension',
+                ),
+                'mimetype' => array(
+                    'location' => 'header',
+                    'type' => 'string',
+                    'sentAs' => 'x-imbo-originalmimetype',
+                ),
+            ),
         ),
     ),
 );
