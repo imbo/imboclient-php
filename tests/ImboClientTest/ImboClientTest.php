c7b3d6e432e6858e7bda08428cabeacc1d674427
Christer Edvartsen <cogo@starzinger.net>
2014-01-09 21:21:06 +0100

    Updated test

--- tests/ImboClientTest/ImboClientTest.php
+++ tests/ImboClientTest/ImboClientTest.php
@@ -329,11 +329,14 @@ class ImboClientTest extends GuzzleTestCase {
               ->from(123)
               ->to(456)
               ->fields(array('width'))
-              ->sort(array('size'));
+              ->sort(array('size'))
+              ->ids(array('id1', 'id2'))
+              ->checksums(array('checksum1', 'checksum2'));
+
         $response = $this->client->getImages($query);
 
         $request = $this->getMockedRequests()[0];
-        $this->assertSame('http://imbo/users/christer/images.json?page=2&limit=5&metadata=1&from=123&to=456&fields[0]=width&sort[0]=size&accessToken=194daa8283d53cc192f5363b182cdb8d51fcd3a84386a2add96d29efc60a619a', urldecode($request->getUrl()));
+        $this->assertSame('http://imbo/users/christer/images.json?page=2&limit=5&metadata=1&from=123&to=456&fields[0]=width&sort[0]=size&ids[0]=id1&ids[1]=id2&checksums[0]=checksum1&checksums[1]=checksum2&accessToken=a4d5c9b94f9ff4169dc59e42a8eb6052eb49f1199d0ac7a8379688186559c96e', urldecode($request->getUrl()));
     }
 
     public function testCanGetTheShortUrlOfAnImage() {
