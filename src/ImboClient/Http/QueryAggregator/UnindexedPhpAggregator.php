6f775069d780f8b086b676a9ffee777ed8c33fa3
Espen Hovlandsdal <rexxars@gmail.com>
2015-12-02 12:57:22 +0100

    Add docblock to query aggregator

--- src/ImboClient/Http/QueryAggregator/UnindexedPhpAggregator.php
+++ src/ImboClient/Http/QueryAggregator/UnindexedPhpAggregator.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the ImboClient package
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
 namespace ImboClient\Http\QueryAggregator;
 
 use Guzzle\Http\QueryAggregator\QueryAggregatorInterface,
@@ -8,6 +16,8 @@ use Guzzle\Http\QueryAggregator\QueryAggregatorInterface,
  * Aggregates nested query string variables using PHP style [],
  * without any numeric indexes ([] vs [0])
  *
+ * @package Client\Http\QueryAggregator
+ * @author Espen Hovlandsdal <espen@hovlandsdal.com>
  */
 class UnindexedPhpAggregator implements QueryAggregatorInterface {
     public function aggregate($key, $value, QueryString $query) {
